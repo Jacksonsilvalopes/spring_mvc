099be1e2c5b04d988eb70615bc3b9c0a301de0f6
Jackson Silva Lopes <j.a.ckson@hotmail.com>
2024-05-30 03:23:00 -0300

    configurando propriedades de conexão msql e classe Dao generica

---
+++ src/main/java/br/com/jacksweb/domain/Departamento.java
@@ -0,0 +1,36 @@
+package br.com.jacksweb.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "DEPARTAMENTOS")
+public class Departamento extends AbstractEntity<Long> {
+
+    @Column(name = "nome", nullable = false, unique = true, length = 60)
+    private String nome;
+
+
+    @OneToMany(mappedBy = "departamento")
+    private List<Cargo> cargos = new ArrayList<>();
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Cargo> getCargos() {
+        return cargos;
+    }
+}
